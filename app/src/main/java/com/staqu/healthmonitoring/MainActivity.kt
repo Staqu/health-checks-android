bf7254bb99dadcba29e352325d185f622fbe54f3
Ramankit Singh <webianks@gmail.com>
2018-09-02 15:58:31 +0530

    Themed properly and latest support libraries

--- app/src/main/java/com/staqu/healthmonitoring/MainActivity.kt
+++ app/src/main/java/com/staqu/healthmonitoring/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.staqu.healthmonitoring
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+        supportActionBar?.title = ""
     }
 }
